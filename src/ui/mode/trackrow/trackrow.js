62fadcc34283565eb2375e0c5f5d2fb2d363e63f
Unknown <timboie@gmail.com>
2019-05-28 12:02:03 -0400

    trackrowview sequencer only redraw if not mixmode

--- src/ui/mode/trackrow/trackrow.js
+++ src/ui/mode/trackrow/trackrow.js
@@ -76,124 +76,126 @@ export const TrackRowView = observer(class TrackRowView extends Component {
       }
       
       //only update sequencer tracks that are modified
-      if(store.ui.viewMode === 'sequencer' && !store.ui.mixMode){
-        if(prevProps.selectedTrack !== this.props.selectedTrack){
-          return;
-        }
+      if(store.ui.viewMode === 'sequencer'){
+        if(!store.ui.mixMode){
+          if(prevProps.selectedTrack !== this.props.selectedTrack){
+            return;
+          }
 
-        if(prevProps.playbackRate !== this.props.playbackRate){
-          this.init();
-          return;
-        }
+          if(prevProps.playbackRate !== this.props.playbackRate){
+            this.init();
+            return;
+          }
 
-        if(prevProps.bpm !== this.props.bpm){
-          this.init();
-          return;
-        }
+          if(prevProps.bpm !== this.props.bpm){
+            this.init();
+            return;
+          }
 
-        if(prevProps.songLength !== this.props.songLength){
-          this.init();
-          return;
-        }
+          if(prevProps.songLength !== this.props.songLength){
+            this.init();
+            return;
+          }
 
-        //transition eventlistener calls init
-        if(prevProps.viewLength !== this.props.viewLength){
-          return;
-        }
-        
-        //same selected note before and after update
-        if(prevProps.selectedNote && this.props.selectedNote){
-          if(prevProps.selectedNote.id === this.props.selectedNote.id){
-            if(prevProps.selectedNoteDuration !== this.props.selectedNoteDuration || prevProps.selectedNoteOffset !== this.props.selectedNoteOffset){
-              this.init();
-              return;
-            }
-            //updated blank selectednote to same selectedkey (select key, create new note, select same key)
-            if(prevProps.selectedNoteValue !== this.props.selectedNoteValue && prevProps.selectedKey === this.props.selectedKey){
-              this.init();
-              return;
+          //transition eventlistener calls init
+          if(prevProps.viewLength !== this.props.viewLength){
+            return;
+          }
+          
+          //same selected note before and after update
+          if(prevProps.selectedNote && this.props.selectedNote){
+            if(prevProps.selectedNote.id === this.props.selectedNote.id){
+              if(prevProps.selectedNoteDuration !== this.props.selectedNoteDuration || prevProps.selectedNoteOffset !== this.props.selectedNoteOffset){
+                this.init();
+                return;
+              }
+              //updated blank selectednote to same selectedkey (select key, create new note, select same key)
+              if(prevProps.selectedNoteValue !== this.props.selectedNoteValue && prevProps.selectedKey === this.props.selectedKey){
+                this.init();
+                return;
+              }
             }
           }
-        }
 
-        //update previous/current track containing selectednote
-        if(prevProps.selectedNote !== this.props.selectedNote){
-          if(this.props.selectedNote){
-            if(this.props.selectedNote.getPattern().track.id === this.props.track.id){
-              this.bSelectedNote = true;
-              //console.log('selected note this track '  + this.props.track.id)
-              this.init();
-              return;
+          //update previous/current track containing selectednote
+          if(prevProps.selectedNote !== this.props.selectedNote){
+            if(this.props.selectedNote){
+              if(this.props.selectedNote.getPattern().track.id === this.props.track.id){
+                this.bSelectedNote = true;
+                //console.log('selected note this track '  + this.props.track.id)
+                this.init();
+                return;
+              }
+              else if(this.bSelectedNote){
+                //console.log('selected note was in this track, but not anymore: ' + this.props.track.id)
+                this.bSelectedNote = false;
+                this.init();
+                return;
+              }
             }
-            else if(this.bSelectedNote){
-              //console.log('selected note was in this track, but not anymore: ' + this.props.track.id)
+            //note was de-selected
+            else{
+              //console.log('no selected note right now and note was in this track prev: ' + this.props.track.id);
               this.bSelectedNote = false;
               this.init();
               return;
             }
           }
-          //note was de-selected
-          else{
-            //console.log('no selected note right now and note was in this track prev: ' + this.props.track.id);
-            this.bSelectedNote = false;
+          
+          if(prevProps.selectedKey !== this.props.selectedKey){
+            if(this.props.selectedNote){
+              if(this.props.selectedNote.getPattern().track.id === this.props.track.id){
+                this.init();
+                return;
+              }
+            }
+          }
+
+          if(prevProps.selectedPattern !== this.props.selectedPattern){
+            if(prevProps.selectedPattern){
+              if(prevProps.selectedPattern.track.id === this.props.track.id){
+                this.init();
+                return;
+              }
+            }
+
+            if(this.props.selectedPattern){
+              if(this.props.selectedPattern.track.id === this.props.track.id){
+                this.init();
+                return;
+              }
+            }
+          }
+          //update quantization(resolution) change for this track's selected pattern
+          else if(prevProps.selectedPattern && this.props.selectedPattern){
+            if(this.props.selectedPattern.track.id === this.props.track.id){
+              if(prevProps.selectedPatternRes !== this.props.selectedPatternRes){
+                this.init();
+                return;
+              }
+              else if(prevProps.selectedPatternNotes !== this.props.selectedPatternNotes){
+                this.init();
+                return;
+              }
+            }
+          }
+
+          /*      
+          if(prevProps.notes !== this.props.notes){
+            console.log('notes changed for track; ' + this.props.track.id)
+            this.init();
+          }
+          */
+
+          if(prevProps.selectedScene !== this.props.selectedScene){
             this.init();
             return;
           }
-        }
         
-        if(prevProps.selectedKey !== this.props.selectedKey){
-          if(this.props.selectedNote){
-            if(this.props.selectedNote.getPattern().track.id === this.props.track.id){
-              this.init();
-              return;
-            }
+          if(prevProps.selectedGroup !== this.props.selectedGroup){
+            this.init();
+            return;
           }
-        }
-
-        if(prevProps.selectedPattern !== this.props.selectedPattern){
-          if(prevProps.selectedPattern){
-            if(prevProps.selectedPattern.track.id === this.props.track.id){
-              this.init();
-              return;
-            }
-          }
-
-          if(this.props.selectedPattern){
-            if(this.props.selectedPattern.track.id === this.props.track.id){
-              this.init();
-              return;
-            }
-          }
-        }
-        //update quantization(resolution) change for this track's selected pattern
-        else if(prevProps.selectedPattern && this.props.selectedPattern){
-          if(this.props.selectedPattern.track.id === this.props.track.id){
-            if(prevProps.selectedPatternRes !== this.props.selectedPatternRes){
-              this.init();
-              return;
-            }
-            else if(prevProps.selectedPatternNotes !== this.props.selectedPatternNotes){
-              this.init();
-              return;
-            }
-          }
-        }
-
-        /*      
-        if(prevProps.notes !== this.props.notes){
-          console.log('notes changed for track; ' + this.props.track.id)
-          this.init();
-        }
-        */
-
-        if(prevProps.selectedScene !== this.props.selectedScene){
-          this.init();
-          return;
-        }
-      
-        if(prevProps.selectedGroup !== this.props.selectedGroup){
-          this.init();
-          return;
         }
       }
       //all other views
