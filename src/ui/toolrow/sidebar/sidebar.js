99e83af0c8b056032cd27fbf30e161808f6d5817
Unknown <timboie@gmail.com>
2019-06-05 11:43:22 -0400

    browse css borders and z-index

--- src/ui/toolrow/sidebar/sidebar.js
+++ src/ui/toolrow/sidebar/sidebar.js
@@ -4,7 +4,7 @@ import { observer } from "mobx-react";
 export const ToolSideBar = observer(class ToolSideBar extends Component {
     render(){
       return (
-        <div id="divSideBarContainer" style={{height: '100%', width: '20%', position:'fixed', zIndex:3, top:0, right:0, borderLeft:'1px solid rgb(169, 169, 169)'}}>
+        <div id="divSideBarContainer">
             { this.props.content }
         </div>
       )
