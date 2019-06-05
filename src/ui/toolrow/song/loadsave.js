d7792cc6acc86cf747b19dab215a8141971dfed1
Unknown <timboie@gmail.com>
2019-06-05 16:52:56 -0400

    load/save: table and style changes

--- src/ui/toolrow/song/loadsave.js
+++ src/ui/toolrow/song/loadsave.js
@@ -8,82 +8,62 @@ export const LoadSaveModal = observer(class LoadSaveModal extends Component {
     selectedSongId;
   
     componentDidMount(){
-      let self = this;
-      let modal = document.getElementById('myModal');
-      let span = document.getElementsByClassName("close")[0];
-      let list = document.getElementById("ulSongs");
-      let input = document.getElementById("inputSong");
-  
-      window.onclick = function(event) {
-        if (event.target === modal) {
-          self.toggleWindow();
-          //modal.style.display = "none";
+      document.getElementById('modalClose').onclick = () => this.toggleWindow();
+    }
+
+    rowClick = (e) => {
+      this.selectedSongId = e.target.parentNode.id;
+
+      let rows = e.target.parentNode.parentNode.children;
+      for(let i=0; i < rows.length; i++){
+        if(rows[i].id === this.selectedSongId){
+          rows[i].style.backgroundColor = 'rgb(19, 62, 131)';
         }
-      }
-  
-      span.onclick = function() {
-        self.toggleWindow();
-        //modal.style.display = "none";
-      }
-  
-      list.onclick = function(e) {
-        if(e.target && e.target.nodeName === "LI") {
-          if(e.target.style.backgroundColor !== "gray"){
-            e.target.style.backgroundColor = "gray";
-            self.selectedSongTitle = e.target.innerHTML;
-            self.selectedSongId = e.target.id;
-            input.value = self.selectedSongTitle;
-  
-            let listItems = list.getElementsByTagName("li");
-            for (let i=0; i < listItems.length; i++) {
-              if(listItems[i].id !== e.target.id){
-                listItems[i].style.backgroundColor = "white";
-              }
-            }
-          }
-          else{
-            e.target.style.backgroundColor = "white";
-            self.selectedSongTitle = "";
-          }
+        else{
+          rows[i].style.backgroundColor = 'transparent';
         }
       }
     }
+
+    formatDate = (date) => {
+      let monthNames = [ "Jan", "Feb", "Mar","Apr", "May", "Jun", "Jul","Aug", "Sep", "Oct","Nov", "Dec"];
+    
+      let monthIndex = date.getMonth();
+      let day = date.getDate();
+      let year = date.getFullYear().toString().substr(2,2);
+    
+      return day + ' ' + monthNames[monthIndex] + ' ' + year;
+    }
   
-    componentDidUpdate(prevProps, prevState, snapshot){
+    componentDidUpdate(prevProps){
       document.getElementById("inputSong").value = store.settings.title;
-      let self = this;
   
-      store.DBGetAllSongs().then(function(list){
-        list = list.sort(function(a, b){return b.modified - a.modified})
-        document.getElementById("ulSongs").innerHTML = "";
-      
-        if(list){
-          if(list.length > 0){
-            self.songs = list;
-            list.forEach(function(song){
-              let node = document.createElement("LI");
-              node.id = song.id;
-  
-              let textnode = document.createTextNode(song.title);
-              node.appendChild(textnode);
-  
-              /*
-              let spanDate = document.createElement("SPAN");
-              let date = new Date(song.modified);
-              spanDate.textContent = (date.getMonth() + 1) + '/' + date.getDate() + '/' +  date.getFullYear();
-              spanDate.style.float = 'right';
-              node.appendChild(spanDate);
-              */
-             
-              document.getElementById('ulSongs').appendChild(node)
-            })
-          }
+      store.DBGetAllSongs().then(list => {
+        let table = document.getElementById('tableSongs');
+
+        while(table.hasChildNodes()){
+          table.removeChild(table.firstChild);
+        }
+
+        if(list.length > 0){
+          list.sort((a,b) => {return b.modified - a.modified}).forEach(song => {
+            let row = table.insertRow(0);
+            row.id = song.id;
+            row.onclick = this.rowClick;
+
+            let cell1 = row.insertCell(0);
+            cell1.style.width = '80%';
+            cell1.innerHTML = song.title;
+
+            let cell2 = row.insertCell(1);
+            cell2.innerHTML = this.formatDate(new Date(song.modified));
+          })
         }
       });
   
       this.toggleWindow();
     }
-  
+
     toggleWindow = () => {
       let modal = document.getElementById('myModal');
   
@@ -102,7 +82,7 @@ export const LoadSaveModal = observer(class LoadSaveModal extends Component {
   
     btnClickLoad = (e) => {
       if(this.selectedSongId){
-        document.getElementsByClassName("close")[0].click();
+        document.getElementById('modalClose').click();
         sessionStorage.setItem("load_songId", this.selectedSongId);
         window.location.reload();
         //store.DBLoadStore(this.selectedSongId);
@@ -126,19 +106,19 @@ export const LoadSaveModal = observer(class LoadSaveModal extends Component {
       }
     }
   
-    componentWillUnmount(){
-    }
+    componentWillUnmount(){}
   
     render(){
       return (
         <div id="myModal" className="modal">
           <div id="modalContent" className="modal-content">
-            <span className="close">&times;</span>
-            <input id="inputSong" type="text" placeholder="song name" onInput={this.inputText} style={{width:'98%', height:'40px'}}></input>
+            <div style={{width:'100%'}}>
+              <span id='modalClose'>&times;</span>
+              <input id="inputSong" type="text" placeholder="song name" onInput={this.inputText}></input>
+            </div>
             <br/>
-            <div id="modalSongList" style={{position:'absolute', width: '100%', overflowY:'auto'}}>
-              <ul id="ulSongs">
-              </ul>
+            <div id="modalSongList">
+              <table id='tableSongs'></table>
             </div>
             <button id="modalBtnLoad" onClick={this.btnClickLoad} style={{bottom:0, width:'50%', height:'40px', position:'absolute', left:0}}>LOAD</button>
             <button id="modalBtnSave" onClick={this.btnClickSave} style={{bottom:0, width:'50%', height:'40px', position:'absolute', right:0}}>SAVE</button>
