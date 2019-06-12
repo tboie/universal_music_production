e7e50b6aae472f0e7686246bf9f4da84988790d7
Unknown <timboie@gmail.com>
2019-06-11 21:00:49 -0400

    nav home: update video

--- src/ui/toolrow/home/home.js
+++ src/ui/toolrow/home/home.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const ToolHome = props => {
     return(
       <div id="divToolHome" className="divToolRowPanelContainer" style={{color:'white', height:'100%', width:'100%'}}>
-        <iframe title="MainVideo" src="https://www.youtube.com/embed/KgZAsfYck1g" 
+        <iframe title="MainVideo" src="https://www.youtube.com/embed/HXwk6U0B-60" 
           frameBorder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowFullScreen 
             style={{height:'100%', width:'100%', display:'table', margin:'0 auto'}}></iframe>
       </div>
